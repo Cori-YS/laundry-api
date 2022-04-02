e631f5c0c8446cc5d2cc3ad45fc3b96d0b551b8d
Ricardo João António <90958374+Ricardo-1976@users.noreply.github.com>
2022-04-02 08:41:18 +0100

    Create a new avatar

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,12 +1,25 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
+const  uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 // Create new User
 usersRoutes.post("/", createUserController.handle);
+
+// Create a new avatar
+usersRoutes.patch(
+  "/avatar",
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
