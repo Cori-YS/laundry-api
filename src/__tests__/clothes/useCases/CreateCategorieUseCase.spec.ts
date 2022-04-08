3309b8bc7e23f65785288c4368bdae661a71eba6
Ricardo-1976 <ricardojoaoantonio2@gmail.com>
2022-04-08 08:16:12 +0100

    Create admin

--- src/__tests__/clothes/useCases/CreateCategorieUseCase.spec.ts
+++ src/__tests__/clothes/useCases/CreateCategorieUseCase.spec.ts
@@ -1,23 +1,22 @@
-import { Connection, createConnection } from "typeorm";
+import  createConnection from "../../../shared/infra/typeorm/index";
 
 import { CategoriesRepository } from "@modules/clothes/infra/typeorm/repositories/CategoriesRepository";
 import { CreateCategoryUseCase } from "@modules/clothes/useCases/createCategory/CreateCategoryUseCase";
 import { AppError } from "@shared/errors/AppError";
 
-let connection: Connection;
+const connection = createConnection("localhost");
 let createCategoryUseCase: CreateCategoryUseCase;
 let categoriesRepository: CategoriesRepository;
 
-describe("Create a category", () => {
-  beforeEach(async () => {
-    connection = await createConnection();
-
+describe("Create a category", async () => {
+  beforeEach( () => {
     categoriesRepository = new CategoriesRepository();
     createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
   });
 
   afterEach(async () => {
-    await connection.close();
+    //await connection.close();
+     (await connection).close;
   });
 
   it("should be able to create a new category", async () => {
---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,21 @@
+import { hash } from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+
+import  createConnection from "../index";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuidV4();
+  const password = await hash("admin", 8);
+
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, address,"isAdmin", created_at)
+      values('${id}', 'admin', 'admin@rentx.com', '${password}', 'XXXXX',true, 'now()')
+    `
+  );
+
+  connection.close;
+}
+
+create().then(() => console.log("User admin create!"));
