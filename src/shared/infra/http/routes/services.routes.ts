c2a169c8df04cf39a10d7d3284230b8b37fba6a9
Ismélio Cori <ismeliocori@hotmail.com>
2022-04-09 20:31:00 +0100

    Update services.routes.ts

--- src/shared/infra/http/routes/services.routes.ts
+++ src/shared/infra/http/routes/services.routes.ts
@@ -1,4 +1,5 @@
 import { CreateServicesController } from "@modules/clothes/useCases/createServices/CreateServicesController";
+import { DeleteServiceController } from "@modules/clothes/useCases/deleteService/DeleteServiceController";
 import { ListServicesController } from "@modules/clothes/useCases/listServices/ListServicesController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -7,8 +8,10 @@ const servicesRoutes = Router();
 
 const createServicesController = new CreateServicesController();
 const listServicesController = new ListServicesController();
+const deleteServiceController = new DeleteServiceController();
 
 servicesRoutes.post("/", ensureAuthenticated, createServicesController.handle);
 servicesRoutes.get("/", listServicesController.handle);
+servicesRoutes.delete("/:service_id", deleteServiceController.handle);
 
 export { servicesRoutes };
