be35276391c67f862c0cdef4825acdf5084258fa
Ismélio Cori <ismeliocori@hotmail.com>
2022-04-20 18:46:38 +0100

    Update 1650232200879-AlterUserAddisActive.ts

--- src/shared/infra/typeorm/migrations/1650232200879-AlterUserAddisActive.ts
+++ src/shared/infra/typeorm/migrations/1650232200879-AlterUserAddisActive.ts
@@ -1,20 +1,18 @@
-import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
 export class AlterUserAddisActive1650232200879 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "users",
+      new TableColumn({
+        name: "isActive",
+        type: "boolean",
+        default: "true",
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.addColumn(
-        "users",
-        new TableColumn({
-          name: "isActive",
-          type: "boolean",
-          default: "true"
-        })
-      );
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.dropColumn("users", "isActive");
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("users", "isActive");
+  }
 }
