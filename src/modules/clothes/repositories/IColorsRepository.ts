792dd2d58f8fc6eade5a10f3d7c4d0d9992ef9a7
Ismélio Cori <ismeliocori@hotmail.com>
2022-04-20 18:53:04 +0100

    Update IColorsRepository.ts

--- src/modules/clothes/repositories/IColorsRepository.ts
+++ src/modules/clothes/repositories/IColorsRepository.ts
@@ -4,6 +4,7 @@ import { Color } from "../infra/typeorm/entities/Color";
 interface IColorsRepository {
   create({ name }: ICreateColorDTO): Promise<Color>;
   findByName(name: string): Promise<Color>;
+  findById(color_id: string): Promise<Color>;
 }
 
 export { IColorsRepository };
